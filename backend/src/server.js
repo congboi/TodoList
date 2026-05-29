5e895e926f44e2f9f38aaf75d527b594f1511147
Tran van cong <congboi2k5@gmail.com>
2026-05-29 18:08:26 +0700

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import taskRouter from "./routes/tasksRouters.js";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
+import path from "path";
 
 //giúp đọc biến môi trường từ file .env
 //
@@ -13,6 +14,7 @@ dotenv.config();
 //sau khi gọi dotenv.config() thì sẽ load các biến môi trường vào trong process.env
 // nhằm bảo mật thông tin tránh ghi vào đây dễ bị lộ thông tin khi đẩy code lên github, đồng thời cũng giúp dễ dàng thay đổi cấu hình mà ko cần phải sửa code
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -21,13 +23,24 @@ const app = express();
 // middleware này 1 chiều từ client gửi dạng json và phải đi qua middleware này để chuyển đổi sang object để có thể sử dụng được trong các route, sau đó khi trả về kết quả thì ko cần đi qua middleware này nữa vì res.json() sẽ tự động chuyển đổi object sang json để trả về cho client
 // hệ thống kiểm tra xem dữ liệu gửi lên có phải là json hay ko, nếu là json thì sẽ chuyển đổi sang object để có thể sử dụng được trong các route
 app.use(express.json());
-app.use(cors());
+if(process.env.NODE_ENV === 'production'){
+    app.use(cors());
+}
 
 
 //gọi api của tasksrouters 
 //khi muốn đổi tiền tố thì đổi ở đây ko cần phải đổi từng cái 1 trong taskRouter nữa
 
 app.use("/api/tasks", taskRouter);
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 
 //gọi hàm kết nối database
